303de544afb01bcc851bfc885ee7dc8772b4440f
vansh <vanshsonawane11@gmail.com>
2026-07-29 21:27:49 +0530

    Fixed IAM module

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,21 +1,18 @@
 resource "aws_iam_role" "ec2_role" {
-
   name = "${var.project_name}-ec2-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
 
-    Statement = [
-      {
-        Effect = "Allow"
+    Statement = [{
+      Effect = "Allow"
 
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
       }
-    ]
+
+      Action = "sts:AssumeRole"
+    }]
   })
 
   tags = {
@@ -24,210 +21,26 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-
   name = "${var.project_name}-instance-profile"
-
   role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "ssm" {
-
-  role = aws_iam_role.ec2_role.name
-
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "s3" {
-
-  role = aws_iam_role.ec2_role.name
-
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "ecr_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}resource "aws_iam_role" "ec2_role" {
-
-  name = "${var.project_name}-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${var.project_name}-ec2-role"
-  }
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-
-  name = "${var.project_name}-instance-profile"
-
-  role = aws_iam_role.ec2_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ssm" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_role_policy_attachment" "s3" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}resource "aws_iam_role" "ec2_role" {
-
-  name = "${var.project_name}-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${var.project_name}-ec2-role"
-  }
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-
-  name = "${var.project_name}-instance-profile"
-
-  role = aws_iam_role.ec2_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ssm" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_role_policy_attachment" "s3" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}resource "aws_iam_role" "ec2_role" {
-
-  name = "${var.project_name}-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${var.project_name}-ec2-role"
-  }
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-
-  name = "${var.project_name}-instance-profile"
-
-  role = aws_iam_role.ec2_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ssm" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_role_policy_attachment" "s3" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_readonly" {
-
-  role = aws_iam_role.ec2_role.name
-
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
